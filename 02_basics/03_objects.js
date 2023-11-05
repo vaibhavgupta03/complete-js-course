ce76c37b726505b4ca02c38691aa0f1d8e6bd2e7
Vaibhav Gupta <vaibhavgupta6393@gmail.com>
2023-11-06 00:45:56 +0530

    Add object properties and methods to JSUser

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,32 @@
+//singleton
+//Object.create
+
+//object literals
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: 'Vaibhav',
+    [mySym]: "key1",
+    age: 20,
+    email: 'vaibhav@google.com',
+    isLoggedIn: false
+}
+//console.log(JsUser);
+/*
+console.log(JsUser.name);
+console.log(JsUser["name"]);
+console.log(JsUser[mySym]);
+*/
+
+//Object.freeze(JsUser);//Jsuser ki value freez kr dega aur change ni hone dega
+
+JsUser.greeting = function () {
+    console.log("Hello JS User");
+}
+
+JsUser.greetingTwo = function () {
+    console.log(`Hello JS User, ${this.name}`);
+}
+
+console.log(JsUser.greetingTwo());
